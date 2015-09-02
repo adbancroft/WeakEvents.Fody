8d1747b37f7769cc49910d84fe71db1a409ab1ec
adbancroft <adbancroft@users.noreply.github.com>
2015-09-02 17:31:12 -0500

    Move TestINterfaces folder.

---
+++ TestAssemblies/TestInterfaces/IEventSource.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+
+namespace TestInterfaces
+{
+    public delegate void CannotBeMadeWeak(object o, int i /* Not derived from EventArgs */);
+
+    // Used by unit tests to access a weaved version of a class.
+    public interface IEventSource
+    {
+        event EventHandler BasicEvent; // Equivalent of EventHandler<EventArgs>
+        void FireBasicEvent();
+        bool IsBasicEventSubscribed { get; }
+
+        event NotifyCollectionChangedEventHandler NonGenericEvent; // Equivalent of EventHandler<NotifyCollectionChangedEventArgs>
+        void FireNonGenericEvent();
+        bool IsNonGenericEventSubscribed { get; }
+
+        event EventHandler<AssemblyLoadEventArgs> GenericEvent;
+        void FireGenericEvent();
+        bool IsGenericEventSubscribed { get; }
+
+        // The weaver should ignore this as the delegate doesn't conform to the standard (object, EventArgs) pattern
+        event CannotBeMadeWeak CannotBeMadeWeak;
+        void FireCannotBeMadeWeak();
+
+        void SubscribeStaticEvent(EventHandler<AssemblyLoadEventArgs> handler);
+        void UnsubscribeStaticEvent(EventHandler<AssemblyLoadEventArgs> handler);
+        void FireStaticGenericEvent();
+        bool IsStaticGenericEventSubscribed { get; }
+    }
+
+#pragma warning restore 0067
+}
